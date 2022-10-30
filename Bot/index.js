a0ce5f6c657d2492d7d3822479a510b139619e77
MatteoRay <Krac1x@gmail.com>
2022-10-30 17:24:57 +0300

    nw

--- Bot/index.js
+++ Bot/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const token = "5757602714:AAHcx61ejaqt_6xtNXGZSIxhSKDMZZd1XVY";
-const webAppUrl = "https://ya.ru";
+const webAppUrl = "https://stalwart-panda-c183e1.netlify.app";
 const bot = new Telegraf(token, { polling: true });
 const app = express();
 
