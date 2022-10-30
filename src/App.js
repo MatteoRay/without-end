60eda2ea6a68948c8e3747834bbe21292b8a6e2e
MatteoRay <Krac1x@gmail.com>
2022-10-30 17:22:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./Components/Card/Card";
+import Cart from "./Components/Cart/Cart";
+import { useTelegram } from "./hooks/useTelegram";
+const { getData } = require("./db/db");
+const products = getData();
+
+function App() {
+  const [cartItems, setCartItems, tg] = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  });
+
+  const onAdd = (products) => {
+    const exist = cartItems.find((x) => x.id === products.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === products.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...products, quantity: 1 }]);
+    }
+  };
+
+  const onRemove = (products) => {
+    const exist = cartItems.find((x) => x.id === products.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== products.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === products.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
+    }
+  };
+
+  const onCheckout = () => {
+    tg.MainButton.text = "Pay :)";
+    tg.MainButton.show();
+  };
+
+  return (
+    <>
+      <h1 className="heading">Order products</h1>
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
+      <div className="cards__container">
+        {products.map((products) => {
+          return (
+            <Card
+              food={products}
+              key={products.id}
+              onAdd={onAdd}
+              onRemove={onRemove}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import "./Cart.css";
+import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
+import Card from "../Card/Card";
+import { getData } from '../../db/db';
+
+const products = getData();
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
+
+const Cart = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('http://85.119.146.179:8000/web-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [addedItems])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
+    return (
+        <div className={'list'}>
+            {products.map(item => (
+                <Card
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
+        </div>
+    );
+};
+
+
+
+export default Cart;
